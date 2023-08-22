4f28bb0e43fcdc544c524bde9cd34cbbd15560ad
Donovan Raber <donovan.raber@outlook.com>
2023-08-22 08:40:50 -0700

    First Class Functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, -2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers = drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(rate) {
+   return function(fare){
+    return fare * rate
+    }
+}
+
+const fareDoubler = createFareMultiplier(2) 
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, shiftDrivers) {
+    return shiftDrivers(drivers)
+}
